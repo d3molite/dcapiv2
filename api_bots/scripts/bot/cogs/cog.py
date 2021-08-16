a029bc1a0628af8694239d950935f08e235dda0c
Demolite <42088225+d3molite@users.noreply.github.com>
2021-08-16 13:25:03 +0200

    #5 added cog master class

--- api_bots/scripts/bot/cogs/cog.py
+++ api_bots/scripts/bot/cogs/cog.py
@@ -2,12 +2,13 @@ import discord
 from discord.ext import commands, tasks
 
 class COG(commands.Cog):
-    def __init__(self, cog_name, bot_name, bot, embed_color):
+    def __init__(self, cog_name, bot_name, bot, embed_color, prefixes=None):
 
         self.bot = bot
         self.cog_name = cog_name
         self.bot_name = bot_name
-        self.embed_color = embed_embed_color
+        self.embed_color = embed_color
+        self.prefixes = prefixes
 
         print("-"*30)
         print("LOADED COG " + self.cog_name + " ON " + self.bot_name)
@@ -18,6 +19,7 @@ class COG(commands.Cog):
         """uses the bot instance to return a guild object"""
         try:
             return await self.bot.get_guild(guild_id)
+
         except:
             print("Bot " + self.bot_name + " failed to fetch guild of id: " + str(guild_id))
             return None
@@ -26,10 +28,16 @@ class COG(commands.Cog):
         """uses the bot instance to return a channel object from a guild"""
         try:
             return await discord.utils.get(guild.channel, id=channel_id) 
+
         except:
             print("Bot " + self.bot_name + " failed to fetch channel of id: " + str(channel_id) + " from guild: " + str(guild))
             return None
 
-    async def get_user(self, guild: discord.Guild, user_id: int):
-        pass
+    async def get_user(self, guild: discord.Guild, user_id: int) -> discord.User:
+        """uses the bot instance to retrieve a user"""
+        try: 
+            return await discord.utils.get(self.bot.get_all_members(), id=user_id)
+
+        except:
+            print("Bot " + self.bot_name + " failed to fetch user of id: " + str(channel_id) + " from guild: " + str(guild))
 
